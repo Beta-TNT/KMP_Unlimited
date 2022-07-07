4564325a8bb6c99d1dbfe4948c7c3f5ee1dca66f
Beta-TNT <betatnt@gmail.com>
2022-07-07 12:27:28 +0800

    Update kmp_unlimited.py

--- kmp_unlimited.py
+++ kmp_unlimited.py
@@ -8,40 +8,41 @@ def classicStrMatching(patternStr:str):
 
 class kmp_unlimited:
     # classic KMP matching algorithm applied to an unlimited sequence
-    _window: deque  # fixed length queue used as a window, use method: append(), popleft()
-    _next: list     # next array
-    _needle = None  # pattern array
+    _window: deque  # a queue with maxlen equals to the length of pattern, used as a window, the way I use it: append() / popleft()
+    _next: list     # NEXT array
+    _pattern = None # pattern array
     _l = 0          # pattern length
     _i = 0          # classic pointer i
     _j = 0          # classic pointer j
 
-    def __init__(self, needle):
-        def getNext(needle): # calculate next array
-            i, j, l = 2, 0, len(needle)
+    def __init__(self, pattern):
+        def getNext(pattern): # classic NEXT array calculating function.
+            i, j, l = 2, 0, len(pattern)
             next = [0] * l
             while i < l:
-                while j > 0 and needle[i-1] != needle[j]:
+                while j > 0 and pattern[i-1] != pattern[j]:
                     j = next[j]
-                if needle[i-1] == needle[j]:
+                if pattern[i-1] == pattern[j]:
                     j += 1
                 next[i] = j
                 i += 1
             return next
-        self._needle = needle
-        self._next = getNext(needle)
-        self._l = len(needle)
+        self._pattern = pattern
+        self._next = getNext(pattern)
+        self._l = len(pattern)
         self._window = deque(maxlen=self._l)
 
     def feedData(self, inputData):
         self._window.append(inputData)
         self._i += 1
         if not len(self._window) == self._l:
+            # will not start matching until the window is filled
             return None
         else:
             for i in range(self._j, self._l):
-                if not self._needle[i](self._window[i]):
+                if not self._pattern[i](self._window[i]):
                     for j in range(i - self._next[i]):
-                        self._window.popleft() # window move right (pop left)
+                        self._window.popleft() # window slides right (pops left)
                     self._j = self._next[i]
                     return None
             self._j = 0 # reset j pointer
@@ -73,8 +74,8 @@ def standardKmp(haystack: str, needle: str) -> int:
             return i - j
     return -1
 
-strRaw = 'ababaaabaababaaaaaaaaabaabaaa'
-strPattern = 'aaaaaa'
+strRaw = 'ababaaabaabababaaaaaaaabaabaaa'
+strPattern = 'abababa'
 
 print(standardKmp(strRaw, strPattern))
 test=kmp_unlimited(classicStrMatching(strPattern))
